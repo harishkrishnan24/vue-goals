c79ef03221dcea40ce52e0a778e5a8d422330457
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-04-05 12:58:01 +0530

    Added eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,8 @@ module.exports = {
     ],
     rules: {
         'vue/html-indent': 0,
-        'vue/html-self-closing': 0
+        'vue/html-self-closing': 0,
+        'vue/max-attributes-per-line': 0,
+        'vue/html-closing-bracket-newline': 0
     }
 };
